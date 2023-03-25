b25d1df0b8987e4582e97203b9e0229015bd535c
Jesse Shaw <JesseShawCodes@gmail.com>
2023-03-25 18:21:45 -0400

    Initial updates to default app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders initial App Component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Videos/i);
   expect(linkElement).toBeInTheDocument();
 });
